541c942f3f78621922e9bab7c7e0ebe111d3d6c5
Yaroslav <valsorya.krow@gmail.com>
2020-06-11 18:41:54 +0300

    new

---
+++ js/ternarniyop.js
@@ -0,0 +1,20 @@
+'use strict';
+const num = 50;
+
+(num === 50) ? console.log('Ok!') :console.log('Error');
+
+console.ReadLine();
+
+
+const numb = 50;
+
+switch (numb) {
+    case 49:
+        console.log("Error");
+        break;
+    case 50:
+        console.log("True");
+        break;
+    default:
+        console.log('Нет');
+}
